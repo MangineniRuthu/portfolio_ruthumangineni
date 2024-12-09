377fed21abee97e671695c078ec0e2132f56c09f
MangineniRuthu <39689521+MangineniRuthu@users.noreply.github.com>
2024-12-09 17:37:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let tablinks=document.querySelectorAll(".tab-links");
 let tabcontents=document.querySelectorAll(".tab-content");
-
+let sidemenu=document.getElementById("sidemenu");
 
 
 tablinks.forEach(tablink => {
@@ -16,3 +16,29 @@ tablinks.forEach(tablink => {
         
     })
   });
+
+  function openmenu(){
+    sidemenu.style.right="0";
+  }
+
+  function closemenu(){
+    sidemenu.style.right="-200px"
+  }
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw7jeUDN7JerjAKIT8isAD3GyrEGZ4FNdVU_OKRFQPfZI3oUH24vL6mWQ79u4WuU5Uzuw/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg=document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message Sent Successfully"
+        setTimeout(()=>{
+          msg.innerHTML=""
+        },5000)
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
